4b10ddeeafb8c6f832f02bfe506e75a038058e6f
Andre Ribeiro <andrebahia7@gmail.com>
2024-05-27 22:09:32 -0300

    Construção do Request

---
+++ Fina.Core/Configuration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fina.Core
+{
+    public static class Configuration
+    {
+        public const int DefaultPageNumber = 1;
+        public const int DefaultPageSize = 25;
+    }
+}
