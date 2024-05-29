46c4d6c13ec8b969e1c8440afa6e27eea00ead83
Andre Ribeiro <andrebahia7@gmail.com>
2024-05-28 22:52:36 -0300

    Start API

---
+++ Fina.Api/Data/Mappings/CategoryMapping.cs
@@ -0,0 +1,14 @@
+﻿using Fina.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Fina.Api.Data.Mappings
+{
+    public class CategoryMapping : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
